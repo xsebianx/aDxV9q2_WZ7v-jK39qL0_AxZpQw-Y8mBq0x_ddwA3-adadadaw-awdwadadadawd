e2e310fa9998c1bf03346cdbcbfb5439d97e18be
Drakath <67465408+xsebianx@users.noreply.github.com>
2024-10-22 04:17:21 -0300

    Update hwids.lua

--- hwids.lua
+++ hwids.lua
@@ -1,7 +1,7 @@
 return {
     permanentHWIDs = {
         "11",
-        "9005F968-46DF-44FC-9C68-B173D505FF37"
+        "111"
     },
     temporaryHWIDs = {
         "DC61583D-84CD-48E1-8AB3-212434BDC519",
