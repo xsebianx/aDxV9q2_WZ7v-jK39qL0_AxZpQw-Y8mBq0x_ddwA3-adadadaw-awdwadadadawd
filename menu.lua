c8b758347a69f8f279aa567858d6eaeea9ceccbd
xsebianx <67465408+xsebianx@users.noreply.github.com>
2024-10-22 17:18:13 -0300

    update

--- menu.lua
+++ menu.lua
@@ -157,7 +157,7 @@ AimbotButton.AutoButtonColor = false -- Para controlar el color al hacer clic
 AimbotButton.ClipsDescendants = true
 local corner = Instance.new("UICorner") -- Añadir esquinas redondeadas
 corner.CornerRadius = UDim.new(0, 12) -- Radio de las esquinas
-corner.Parent = AimbotButton)
+corner.Parent = AimbotButton
 
 -- Botón Aimbot NPC
 AimbotNPCButton = Instance.new("TextButton") -- Crea el botón
@@ -181,6 +181,18 @@ ESPButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 ESPButton.BackgroundColor3 = Color3.fromRGB(75, 75, 75)
 ESPButton.Size = UDim2.new(0, 240, 0, 40)
 ESPButton.Position = UDim2.new(0, 10, 0, 10)
+
+-- Funcionalidades de Visual
+local VisorButton = Instance.new("TextButton") -- Asegúrate de crear la instancia del botón
+VisorButton.Name = "VisorButton" -- Cambié el nombre aquí
+VisorButton.Parent = VisualFrame
+VisorButton.Text = "Visor: Off" -- Cambié el texto para que se refiera al visor
+VisorButton.Font = Enum.Font.GothamBold
+VisorButton.TextSize = 18
+VisorButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+VisorButton.BackgroundColor3 = Color3.fromRGB(75, 75, 75)
+VisorButton.Size = UDim2.new(0, 240, 0, 40)
+VisorButton.Position = UDim2.new(0, 10, 0, 60) -- Cambié la posición para que esté debajo de ESP
 
 -- Funcionalidades de Extra
 HackDetectorButton.Name = "HackDetectorButton"
@@ -250,6 +262,20 @@ ESPButton.MouseButton1Click:Connect(function()
         ESPButton.Text = "ESP: Off"
         if _G.disableESP then
             _G.disableESP() -- Desactivar el ESP
+        end
+    end
+end)
+
+local visorEnabled = false
+VisorButton.MouseButton1Click:Connect(function()
+    visorEnabled = not visorEnabled
+    if visorEnabled then
+        VisorButton.Text = "Visor: On"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xsebianx/awdadadawwadwadabadBVWBRwqddadda-adadadaw-awdwadadadawd/refs/heads/main/visual/visor.lua"))() -- Reemplaza "tu_url_de_visor_aqui" con la URL correspondiente
+    else
+        VisorButton.Text = "Visor: Off"
+        if _G.disableVisor then
+            _G.disableVisor() -- Desactivar el visor
         end
     end
 end)
