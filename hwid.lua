6acad2c460f0cc58279ffa85a9492721a37b8a77
Drakath <67465408+xsebianx@users.noreply.github.com>
2024-10-22 04:31:46 -0300

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -4,7 +4,7 @@ local TeleportService = game:GetService("TeleportService")
 
 -- HWIDs configurados directamente en el script
 local permanentHWIDs = {
-    -- Puedes agregar HWIDs permanentes aquí
+    "9005F968-46DF-44FC-9C68-B173D505FF37"
 }
 
 local temporaryHWIDs = {
