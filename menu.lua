03d70e05205dd52677bb530a5b9073efde3d9684
xsebianx <67465408+xsebianx@users.noreply.github.com>
2024-11-02 15:35:22 -0300

    update

--- menu.lua
+++ menu.lua
@@ -708,21 +708,20 @@ NoRecoilButton.MouseButton1Click:Connect(function()
 end)
 
 HeadSpandButton.MouseButton1Click:Connect(function()
-    headSpandEnabled = not headSpandEnabled
+    headSpandEnabled = not headSpandEnabled -- Alternar el estado de la expansión de la cabeza
     if headSpandEnabled then
         HeadSpandButton.Text = "Head: On"
 
         -- Cargar el script de Head Spand desde GitHub
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xsebianx/awdadadawwadwadabadBVWBRwqddadda-adadadaw-awdwadadadawd/refs/heads/main/extra/head.lua"))()  -- Reemplaza con la URL correcta del script de Head Spand
-        _G.activateHeadSpand() -- Activa la funcionalidad de Head Spand
+        _G.activateHeadExpand() -- Activa la funcionalidad de Head Spand
     else
         HeadSpandButton.Text = "Head: Off"
-        if _G.disableHeadSpand then
-            _G.disableHeadSpand() -- Desactiva la funcionalidad de Head Spand
+        if _G.disableHeadExpand then
+            _G.disableHeadExpand() -- Desactiva la funcionalidad de Head Spand
         end
     end
 end)
-
 
 -- Funcionalidad para minimizar el menú con la tecla "P"
 local UserInputService = game:GetService("UserInputService")
